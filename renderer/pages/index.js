038fdd31c80ec1b90428280f8457aac6fac88dde
kubarium <iam@denizkumsal.com>
2018-02-05 17:13:31 -0500

    primes it with primereact

--- renderer/pages/index.js
+++ renderer/pages/index.js
@@ -1,12 +1,17 @@
+import Head from 'next/head'
 import Link from 'next/link'
+import { Menubar } from 'primereact/components/menubar/Menubar';
+import menu from '../menu.json'
 
 export default () => (
     <div>
-      <span>This is Next.js speaking</span>
-      <p>
-        <Link href="/about">
-        <a>About</a>
-        </Link>
-      </p>
+      <Head>
+        <title>Viroid Games Tools</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.css" />
+        <link rel="stylesheet" href="/static/primereact.min.css" />
+        <link rel="stylesheet" href="/static/theme.css" />
+        <script defer src="https://use.fontawesome.com/releases/v5.0.0/js/all.js"></script>
+      </Head>
+      <Menubar model={ menu } />
     </div>
 )
