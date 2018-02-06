9a3713283bc9c7e8fa29e97ec067f5725e7506c6
kubarium <iam@denizkumsal.com>
2018-02-06 17:10:18 -0500

    404 page

---
+++ renderer/pages/_error.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default () => <div>
+                       <p>
+                         This feature has not been implemented yet.
+                       </p>
+                       <p>
+                         Energize from the Meta menu.
+                       </p>
+                     </div>
