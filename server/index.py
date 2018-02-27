51a4843c1e326e154e94c4057613a5b8dfeb69d5
Kumsal Obuz <iam@denizkumsal.com>
2018-02-27 00:35:09 -0500

    normalizes file path between Windows and Mac

--- server/index.py
+++ server/index.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 from utils import *
-
+import os
 
 @app.route("/post/<path:book>")
 def hello(book):
-    return str(readFile("/" + book))
+    return str(readFile(os.path.abspath(book)))
