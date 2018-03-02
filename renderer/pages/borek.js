159e87aa0e315ef87b0e95fd0a41a6bbabaa6122
kubarium <iam@denizkumsal.com>
2018-03-02 17:30:47 -0500

    base component

--- renderer/pages/borek.js
+++ renderer/pages/borek.js
@@ -1,7 +1,27 @@
-import Link from 'next/link'
+import Layout from "../components/Layout"
+import { pathDeconstructor } from "../components/utils"
 
-export default () => <div>
-                       <p>
-                         Borek
-                       </p>
-                     </div>
+import React, { Component } from 'react'
+
+export default class extends Component {
+  static async getInitialProps({req, pathname}) {
+    return {
+      //title: title(pathname)
+    }
+  }
+  render() {
+
+    return (
+      <Layout title={ pathDeconstructor(this.props.url.pathname) }>
+        <div>Do borek</div>
+        <style jsx>
+          { `
+    div {
+        border:1px dashed blue;
+    }
+    ` }
+        </style>
+      </Layout>
+    )
+  }
+}
