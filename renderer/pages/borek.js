74208dbf5254f9752ee8403d9577acb8bcfc2e6f
Kumsal Obuz <iam@denizkumsal.com>
2018-03-04 13:07:55 -0500

    introduces text editor

--- renderer/pages/borek.js
+++ renderer/pages/borek.js
@@ -1,7 +1,19 @@
-import Link from 'next/link'
+import AceEditor from 'react-ace';
+import brace from 'brace'
 
+
+import {lipsum} from '../static/lipsum'
+
+import 'brace/mode/markdown';
+import 'brace/snippets/markdown';
+import 'brace/ext/language_tools';
+import 'brace/theme/github';
+import Link from 'next/link'
+function onChange(newValue) {
+  console.log('change', newValue);
+}
 export default () => <div>
                        <p>
-                         Borek
+                         <AceEditor mode="markdown" theme="github" onChange={ onChange } name="UNIQUE_ID_OF_DIV" editorProps={ { $blockScrolling: true } } value={ lipsum(5) } />
                        </p>
                      </div>
